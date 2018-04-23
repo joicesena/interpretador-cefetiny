8f1e4788f5d2d42a5a95c2138bee58aa4884e8c4
aluno <livia.delgado.ac@gmail.com>
2018-04-23 14:49:55 -0300

    Atualiza classe Executor

--- src/br/cefetmg/inf/tiny/executor/Executor.java
+++ src/br/cefetmg/inf/tiny/executor/Executor.java
@@ -1,6 +1,11 @@
 package br.cefetmg.inf.tiny.executor;
 
 import br.cefetmg.inf.tiny.estruturasDados.Fila;
+import br.cefetmg.inf.tiny.estruturasDados.Pilha;
+import br.cefetmg.inf.tiny.excecoes.ExcecaoFilaVazia;
+import br.cefetmg.inf.tiny.excecoes.ExcecaoPilhaVazia;
+import br.cefetmg.inf.tiny.executor.comandos.*;
+import br.cefetmg.inf.util.Dicionarios;
 
 /**
  * tirar da fila de execução principal e adicionar numa fila temporária do comando
@@ -12,5 +17,129 @@ public final class Executor {
     
     public static void executaPrograma(Fila filaAnalisada) {
         filaExecucao = filaAnalisada;
+        
+        try {
+            Object temp = null;        
+            while (!filaExecucao.filaVazia()) {
+                temp = filaExecucao.removeFila();
+                if (Dicionarios.procuraElementoNoDicionario((String)temp, Dicionarios.LISTA_COMANDOS)) {
+                    // é um comando
+                    Comando comando;
+                    Object elementoFilaExecucao;
+                    switch (Dicionarios.qualComando((String)temp)) {
+                        // print
+                        case 0:
+                            comando = new ComandoPrint((String)filaExecucao.removeFila());
+                            break;
+                        // println
+                        case 1:
+                            comando = new ComandoPrintln((String)filaExecucao.removeFila());
+                            break;
+                        //"readInt",    2
+                        case 2:
+                            comando = new ComandoReadInt((String)filaExecucao.removeFila());
+                            break;
+                        //"if",         3
+                        case 3:
+                            
+                            Pilha pilhaIf = new Pilha();
+                            pilhaIf.empilha(temp);
+                            
+                            Fila filaComandoIf = new Fila();
+                            filaComandoIf.insereFila(temp);
+                            
+                            boolean acabouIf = false;
+                            
+                            while (!acabouIf) {
+                                elementoFilaExecucao = filaExecucao.removeFila();
+                                filaComandoIf.insereFila(elementoFilaExecucao);
+                                if (((String)elementoFilaExecucao).equals("endif")) {
+                                    pilhaIf.desempilha();
+                                    if (pilhaIf.pilhaVazia())
+                                        acabouIf = true;
+                                } else if (((String)elementoFilaExecucao).equals("if")) {
+                                    pilhaIf.empilha(elementoFilaExecucao);
+                                }
+                            }
+                            
+                            comando = new ComandoIf(filaComandoIf);
+                            
+                            break;
+                        //"while",      7
+                        case 7:
+                            Pilha pilhaWhile = new Pilha();
+                            pilhaWhile.empilha(temp);
+                            
+                            Fila filaComandoWhile = new Fila();
+                            filaComandoWhile.insereFila(temp);
+                            
+                            boolean acabouWhile = false;
+                            
+                            while (!acabouWhile) {
+                                elementoFilaExecucao = filaExecucao.removeFila();
+                                filaComandoWhile.insereFila(elementoFilaExecucao);
+                                if (((String)elementoFilaExecucao).equals("endwhile")) {
+                                    pilhaWhile.desempilha();
+                                    if (pilhaWhile.pilhaVazia())
+                                        acabouIf = true;
+                                } else if (((String)elementoFilaExecucao).equals("while")) {
+                                    pilhaWhile.empilha(elementoFilaExecucao);
+                                }
+                            }
+                            
+                            comando = new ComandoWhile(filaComandoWhile);
+                            
+                            break;
+                        //"for",        10
+                        case 10:
+                            Pilha pilhaFor = new Pilha();
+                            pilhaFor.empilha(temp);
+                            
+                            Fila filaComandoFor = new Fila();
+                            filaComandoFor.insereFila(temp);
+                            
+                            boolean acabouFor = false;
+                            
+                            while (!acabouFor) {
+                                elementoFilaExecucao = filaExecucao.removeFila();
+                                filaComandoFor.insereFila(elementoFilaExecucao);
+                                if (((String)elementoFilaExecucao).equals("endfor")) {
+                                    pilhaFor.desempilha();
+                                    if (pilhaFor.pilhaVazia())
+                                        acabouIf = true;
+                                } else if (((String)elementoFilaExecucao).equals("for")) {
+                                    pilhaFor.empilha(elementoFilaExecucao);
+                                }
+                            }
+                            
+                            comando = new ComandoFor(filaComandoFor);
+                            
+                            break;
+                        // end
+                        case 14:
+                            System.exit(0);
+                            break;
+                    }
+                } else {
+                    // atribuição
+                    Fila filaComandoAtribuicao = new Fila();
+
+                    // nome da variável
+                    filaComandoAtribuicao.insereFila(temp);
+
+                    // atribuidor
+                    temp = filaExecucao.removeFila();
+                    filaComandoAtribuicao.insereFila(temp);
+
+                    // valor da variável
+                    temp = filaExecucao.removeFila();
+                    filaComandoAtribuicao.insereFila(temp);
+                }
+            }
+        } catch (ExcecaoFilaVazia ex) {
+            System.err.println(ex.getMessage());
+        } catch (ExcecaoPilhaVazia ex) {
+            System.err.println(ex.getMessage());
+        }
     }
 }
