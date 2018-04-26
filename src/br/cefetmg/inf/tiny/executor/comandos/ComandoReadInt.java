aba2180af41be386d238e7707e06610c6b149ae9
unknown <joice.sena.502@gmail.com>
2018-04-25 21:28:05 -0300

    Remove exceções desnecessárias em ComandoReadInt

--- src/br/cefetmg/inf/tiny/executor/comandos/ComandoReadInt.java
+++ src/br/cefetmg/inf/tiny/executor/comandos/ComandoReadInt.java
@@ -5,15 +5,13 @@ import br.cefetmg.inf.tiny.memoria.EstruturaMemoria;
 import br.cefetmg.inf.tiny.estruturasDados.Fila;
 import java.util.InputMismatchException;
 import br.cefetmg.inf.tiny.excecoes.ExcecaoExpressaoInvalida;
-import br.cefetmg.inf.tiny.excecoes.ExcecaoFilaVazia;
-import br.cefetmg.inf.tiny.excecoes.ExcecaoPilhaVazia;
 
 public final class ComandoReadInt extends Comando{
     Scanner entrada = new Scanner(System.in);
     int valorRecebido;
     String nomeVariavel = null;
 
-    public ComandoReadInt(String parametro) throws ExcecaoPilhaVazia, ExcecaoFilaVazia, ExcecaoExpressaoInvalida {
+    public ComandoReadInt(String parametro) throws ExcecaoExpressaoInvalida {
         super(parametro); 
         this.analisa();
     }
