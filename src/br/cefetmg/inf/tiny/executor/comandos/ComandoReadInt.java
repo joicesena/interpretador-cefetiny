724389aeb8dc2ddea053480a204535c7d7e004d2
unknown <joice.sena.502@gmail.com>
2018-04-25 21:22:40 -0300

    Atualiza ComandoReadInt

--- src/br/cefetmg/inf/tiny/executor/comandos/ComandoReadInt.java
+++ src/br/cefetmg/inf/tiny/executor/comandos/ComandoReadInt.java
@@ -1,14 +1,21 @@
 package br.cefetmg.inf.tiny.executor.comandos;
 
+import java.util.Scanner;
+import br.cefetmg.inf.tiny.memoria.EstruturaMemoria;
 import br.cefetmg.inf.tiny.estruturasDados.Fila;
+import java.util.InputMismatchException;
 import br.cefetmg.inf.tiny.excecoes.ExcecaoExpressaoInvalida;
 import br.cefetmg.inf.tiny.excecoes.ExcecaoFilaVazia;
 import br.cefetmg.inf.tiny.excecoes.ExcecaoPilhaVazia;
 
-public class ComandoReadInt extends Comando{
+public final class ComandoReadInt extends Comando{
+    Scanner entrada = new Scanner(System.in);
+    int valorRecebido;
+    String nomeVariavel = null;
 
-    public ComandoReadInt(String parametro) {
-        super(parametro);
+    public ComandoReadInt(String parametro) throws ExcecaoPilhaVazia, ExcecaoFilaVazia, ExcecaoExpressaoInvalida {
+        super(parametro); 
+        this.analisa();
     }
 
     public ComandoReadInt(Fila filaComandoAtual) {
@@ -16,11 +23,21 @@ public class ComandoReadInt extends Comando{
     }
 
     @Override
-    public void executaComando() {
+    public void executaComando() throws ExcecaoExpressaoInvalida {
+        nomeVariavel = parametro.substring(1, parametro.length()-1); 
+        variaveis = EstruturaMemoria.getInstancia();
+        variaveis.armazenaVariavel(nomeVariavel, valorRecebido);
     }
 
     @Override
-    public void analisa() throws ExcecaoPilhaVazia, ExcecaoFilaVazia, ExcecaoExpressaoInvalida {
+    public void analisa() throws ExcecaoExpressaoInvalida {
+        try {
+            valorRecebido = entrada.nextInt();   
+        } catch(InputMismatchException e) {
+            System.err.println("\nErro: A entrada recebida não é do tipo inteiro.");
+            System.exit(0);
+        } 
+        this.executaComando();
     }
     
 }
