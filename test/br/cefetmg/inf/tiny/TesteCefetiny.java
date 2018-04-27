918317ba736fd5fe9cb9b53d44c4baddfda2b4c7
LivDelgado <livia.delgado.ac@gmail.com>
2018-04-27 15:55:38 -0300

    Corrige TesteCefetiny

--- test/br/cefetmg/inf/tiny/TesteCefetiny.java
+++ test/br/cefetmg/inf/tiny/TesteCefetiny.java
@@ -2,18 +2,16 @@ package br.cefetmg.inf.tiny;
 
 import br.cefetmg.inf.tiny.analisador.AnalisadorSintatico;
 import br.cefetmg.inf.tiny.entradaCodigo.LeitorArquivo;
-import br.cefetmg.inf.tiny.estruturasDados.Pilha;
 import br.cefetmg.inf.tiny.excecoes.ExcecaoEntradaInvalida;
 import br.cefetmg.inf.tiny.excecoes.ExcecaoErroSintatico;
 import br.cefetmg.inf.tiny.excecoes.ExcecaoExpressaoInvalida;
 import br.cefetmg.inf.tiny.excecoes.ExcecaoFilaVazia;
+import br.cefetmg.inf.tiny.excecoes.ExcecaoListaVazia;
 import br.cefetmg.inf.tiny.excecoes.ExcecaoPilhaVazia;
 import br.cefetmg.inf.tiny.memoria.EstruturaMemoria;
-import br.cefetmg.inf.tiny.memoria.Variavel;
 import java.io.FileNotFoundException;
 import org.junit.After;
 import org.junit.AfterClass;
-import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
@@ -47,7 +45,7 @@ public class TesteCefetiny {
     }
 
     @Test
-    public void testeCefetiny() throws ExcecaoPilhaVazia, ExcecaoExpressaoInvalida, FileNotFoundException, ExcecaoFilaVazia, ExcecaoErroSintatico, ExcecaoEntradaInvalida {
+    public void testeCefetiny() throws ExcecaoPilhaVazia, ExcecaoExpressaoInvalida, FileNotFoundException, ExcecaoFilaVazia, ExcecaoErroSintatico, ExcecaoEntradaInvalida, ExcecaoListaVazia {
         LeitorArquivo.leArquivo("test/br/cefetmg/inf/tiny/arqTestes/teste1.txt");
         AnalisadorSintatico aS = AnalisadorSintatico.getInstancia(LeitorArquivo.getCodigo());
         assert true;
